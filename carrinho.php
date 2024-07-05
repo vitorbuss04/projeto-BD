ab9fe037737be69473917cb887f8f1d6f77a9d00
Vítor Augusto Buss <109056455+vitorbuss04@users.noreply.github.com>
2024-07-05 10:58:17 -0300

    estilo da pagina de livros completo

--- carrinho.php
+++ carrinho.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
     require 'conexao.php';
 ?>
 <!DOCTYPE html>
@@ -6,62 +6,85 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link rel="stylesheet" href="styles/carrinho.css">
-    <link rel="stylesheet" href="">
-    <title>Biblioteca Emil</title>
 </head>
 <body>
-    <header>
-        <img src="img/logo.svg" alt="logo" width="50px">
-        <h1>Livraria do Buss</h1>
-    </header>
-    <main>
-        <a href="index.php" class="voltar">HOME</a>
-    <table>
-        <thead>
-            <tr>
-                <th>id produto</th>
-                <th>titulo</th>
-                <th>valor</th>
-                <th>quantidade</th>
-                <th>ação</th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php 
-            $sql = 'SELECT * FROM carrinho';
-            $itens = mysqli_query($conexao, $sql);
-            if(mysqli_num_rows($itens) > 0) {
-                foreach($itens as $item) {
-        ?>
-        <tr>
-            <td><?= $item['id_produto'] ?></td>
-            <td><?= $item['nome_produto'] ?></td>
-            <td><?= $item['valor'] ?></td>
-            <td><?= $item['quant'] ?></td>
-            <td>
-                <form action="acoes.php" method="POST" style="padding: 0;">
-                    <button type="submit" name="delete_cart_item" value="<?=$item['id_item']?>">Excluir</button>
-                </form>
-            </td>
-        </tr>
-        <?php 
-                }
-            } else {
-                echo "<h5>Nenhum livro encontrado.</h5>";
-            }
-        ?>
-    </tbody>
+    <header style="z-index: 999;">
 
-    </table>
-    <div class="card_total">
-        <?php 
-            $sql = "SELECT SUM(valor) as total FROM carrinho";
-            $query = mysqli_query($conexao, $sql);
-            $total = mysqli_fetch_array($query);
-        ?>
-        <p>Total: R$<?= $total['total'] ?></p>
-    </div>
+        <div class="container-logo">
+            <img src="img/logo.svg" alt="Logo" class="logo-img">
+            <h1 class="logo-texto">Livraria do Buss</h1>
+        </div>
+
+        <div class="container-buttons">
+
+            <a href="adicionar-livro.php" class="botao">
+                <div class="botao-content">
+                    <img src="img/plus.svg" alt="icone adicionar" class="button-icon">
+                    <p class="button-texto">Adicionar</p>
+                </div>
+            </a>
+
+            <a href="carrinho1.php" class="botao">
+                <div class="botao-content">
+                    <img src="img/carrinho.svg" alt="icone carrinho" class="button-icon">
+                    <p class="button-texto">Carrinho</p>
+                </div>
+            </a>
+
+        </div>
+    </header>
+    
+    <main>
+        <a href="index.php" class="voltar">Voltar</a>
+        <table>
+            <thead>
+                <tr>
+                    <th>id produto</th>
+                    <th>titulo</th>
+                    <th>valor</th>
+                    <th>quantidade</th>
+                    <th>ação</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    $sql = 'SELECT * FROM carrinho';
+                    $itens = mysqli_query($conexao, $sql);
+                    if(mysqli_num_rows($itens) > 0) {
+                        foreach($itens as $item) {
+                ?>
+                <tr>
+                    <td><?= $item['id_produto'] ?></td>
+                    <td><?= $item['nome_produto'] ?></td>
+                    <td><?= $item['valor'] ?></td>
+                    <td><?= $item['quant'] ?></td>
+                    <td>
+                        <form action="acoes.php" method="POST" style="padding: 0;">
+                            <button type="submit" name="delete_cart_item" value="<?=$item['id_item']?>"> <img src="img/deletar.svg" alt="excluir" class="delete-img"> Excluir</button>
+                        </form>
+                    </td>
+                </tr>
+                <?php 
+                        }
+                    } else {
+                        echo "<h5>Nenhum livro encontrado.</h5>";
+                    }
+                ?>
+            </tbody>
+    
+        </table>
+        <div class="total">
+            <?php 
+                $sql = "SELECT SUM(valor) as total FROM carrinho";
+                $query = mysqli_query($conexao, $sql);
+                $total = mysqli_fetch_array($query);
+                
+            ?>
+            <p class="total-text">Total: R$<?= $total['total'] ?></p>
+        </div>
     </main>
+
 </body>
 </html>
--- visualizar-livro.php
+++ visualizar-livro.php
@@ -6,50 +6,74 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Livros preferidos</title>
-    <link rel="stylesheet" href="styles/style.css">
+    <title>Visualizar livro</title>
     <link rel="stylesheet" href="styles/visualizar.css">
 </head>
 <body>
     <header>
-        <nav>
-            <h1>Livros preferidos</h1>
-        </nav>
-    </header>
-    <main>
-        <div class="voltar">
-            <h2>Visualizar Livro</h2>
-            <a href="index.php" class="link_voltar">Voltar</a>
-        </div>
-        <div class="info_livro">
 
-            <?php 
-                if(isset($_GET['id'])) {
-                    $livro_id = mysqli_real_escape_string($conexao, $_GET['id']);
-                    $sql = "SELECT * FROM livros WHERE id = $livro_id";
-                    $query = mysqli_query($conexao, $sql);
+    <div class="container-logo">
+        <img src="img/logo.svg" alt="Logo" class="logo-img">
+        <h1 class="logo-texto">Livraria do Buss</h1>
+    </div>
 
-                    if(mysqli_num_rows($query) > 0) {
-                        $livro = mysqli_fetch_array($query);
-                    
-                
-            ?>
-            <div class="capa">
-                <img src="<?= $livro['capa'] ?>" width="200px" alt="Capa: <?= $livro['capa'] ?>">
+    <div class="container-buttons">
+
+        <a href="adicionar-livro.php" class="botao">
+            <div class="botao-content">
+                <img src="img/plus.svg" alt="icone adicionar" class="button-icon">
+                <p class="button-texto">Adicionar</p>
             </div>
-            <div class="dados_livro">
-                <div class="descricao">
-                    <h2 class="titulo"><?= $livro['titulo'] ?></h2>
-                    <p class="autor"><?= $livro['autor'] ?></p>
-                    <p class="genero">Gênero <?= $livro['genero'] ?></p>
-                    <p class="isbn">ISBN: <?= $livro['isbn'] ?></p>
-                    <p class="valor">R$<?= $livro['valor'] ?></p>
+        </a>
+
+        <a href="carrinho1.php" class="botao">
+            <div class="botao-content">
+                <img src="img/carrinho.svg" alt="icone carrinho" class="button-icon">
+                <p class="button-texto">Carrinho</p>
+            </div>
+        </a>
+
+    </div>
+    </header>
+
+    <main>
+        <div class="container-voltar">
+
+            <a href="index.php" class="voltar-button">Voltar</a>
+
+        </div>
+        <div class="info-produto">
+            <?php 
+                    if(isset($_GET['id'])) {
+                        $livro_id = mysqli_real_escape_string($conexao, $_GET['id']);
+                        $sql = "SELECT * FROM livros WHERE id = $livro_id";
+                        $query = mysqli_query($conexao, $sql);
+
+                        if(mysqli_num_rows($query) > 0) {
+                            $livro = mysqli_fetch_array($query);
+            ?>
+
+            <div class="content-produto">
+
+                <div class="container-img">
+                    <img src="<?= $livro['capa'] ?>" alt="" class="capa-livro">
                 </div>
-                <form action="acoes.php" method="post" style="padding: 0;">
-                    <button type="submit" value="<?= $livro['id'] ?>" name="add_cart" class="adicionar">Adicionar ao carrinho</button>
-                    <input type="number" name="quant" class="quantidade" placeholder="Quantidade" value="1">
-                </form>
-                
+
+                <div class="container-informacoes">
+                    <div class="informacoes-textos">
+                        <h3 class="informacoes-titulo"><?= $livro['titulo'] ?></h3>
+                        <p class="informacoes-autor"><?= $livro['autor'] ?></p>
+                        <p class="informacoes-genero"><?= $livro['genero'] ?></p>
+                        <p class="informacoes-valor"><?= $livro['valor'] ?></p>
+                    </div>
+
+                    <form action="acoes.php" method="post" class="container-adicionar" style="padding: 0;">
+                        <button type="submit" class="botao-adicionar" name="add_cart" value="<?= $livro['id'] ?>" >Adicionar ao carrinho</button>
+
+                        <input type="number" class="quantidade" value="<?= $livro['id'] ?>" name="quant">
+                    </form>
+                </div>
+
             </div>
             <?php 
                     } else {
@@ -57,6 +81,7 @@
                     }
                 }
             ?>
+
         </div>
     </main>
 </body>
